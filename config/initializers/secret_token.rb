5a7b266dd0bf2c29643971bb0f002f1164bbbbb1
vinaybl7 <vinaybl@gmail.com>
2013-07-31 16:23:22 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Chinnu::Application.config.secret_key_base = '661d11b3adc0993f5f4a2527e0865bb9c08cddafcbfb3ef1467828a9742cce15cb44f87dc71ad6e84693a38620798d17e0b99a7c2d656552964c53ff31aadb73'
